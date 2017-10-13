671c9032268bb1a7034681b5f68b6e9de6a94795
Marek Podsiadły <marekpodsiadly@gmail.com>
2017-10-13 22:23:51 +0200

    unused code removed

--- tools/common.js
+++ tools/common.js
@@ -2,26 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const gpxParse = require('gpx-parse');
 
-const processFile = (filePath) => {
-    return new Promise((resolve, reject) => {
-        gpxParse.parseGpxFromFile(filePath, function(error, data) {
-            if (error) {
-                console.error(error);
-                reject(error);
-                return;
-            }
-            
-            resolve(data);
-        });
-    });
-};
-
-const parseGpxFiles = (files) => {
-    return Promise.all(files.map((file) => {
-        return processFile(file);
-    }));
-};
-
 const listFilesInDir = (dirPath) => {
     return new Promise((resolve, reject) => {
         fs.readdir(dirPath, (error, files) => {
@@ -40,60 +20,6 @@ const listFilesInDir = (dirPath) => {
     });
 };
 
-const extractRoutes = (datas) => {
-    return new Promise((resolve) => {
-        let routes = [];
-        datas.forEach((data) => {
-            if (!data.routes) {
-                console.warn('No routes!');
-                return;
-            }
-
-            data.routes.forEach((route) => {
-                routes.push(route);
-            });
-        });
-
-        resolve(routes);
-    });
-};
-
-const createPassObject = (waypoint) => {
-    let splitted = waypoint.name.split('/');
-    
-    const name = splitted[0].trim();
-    const elevation = splitted[1].trim();
-    const slope = splitted[2].trim();
-
-    return {
-        name: name,
-        point: { 
-            lat: waypoint.lat, 
-            lon: waypoint.lon 
-        },
-        elevation: elevation,
-        slope: slope
-    };
-};
-
-const writeToFile = (path, content) => {
-    return new Promise((resolve, reject) => {
-        const json = JSON.stringify(content, null, 2);
-
-        fs.writeFile(path, json, 'utf-8', (error) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve();
-            }
-        });
-    });
-}
-
 module.exports = {
-    listFilesInDir: listFilesInDir,
-    parseGpxFiles: parseGpxFiles,
-    extractRoutes: extractRoutes,
-    createPassObject: createPassObject,
-    writeToFile: writeToFile
+    listFilesInDir: listFilesInDir
 }
